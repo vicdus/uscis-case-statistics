14bc65e52a89f79410056b5370eaefc6a6ac7572
Moyang <iammoyang@gmail.com>
2020-05-12 20:50:15 -0700

    add clawer script

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import data from "./data.json";
 
 function App() {
-  return <div>helloworld</div>;
+  console.log(data);
+
+  const entires = Object.entries(data).map(([key, count]) => {
+    const [center, year, day, code, form, status, timestamp] = key.split("|");
+    return { center, year, day, code, form, status, timestamp, count };
+  });
+
+  return (
+    <div>
+      {entires.map((e) => (
+        <div>{JSON.stringify(e)}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/clawer.py
@@ -0,0 +1,91 @@
+import requests
+import asyncio
+from bs4 import BeautifulSoup
+import re
+from collections import Counter
+import json
+from datetime import date
+
+BASE_URL = 'https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum='
+
+# see https://egov.uscis.gov/processing-times/
+FORM_TYPES = [
+    'I-90', 'I-102', 'I-129', 'I-129CW', 'I-129F', 'I-130', 'I-131', 'I-140', 'I-212', 'I-360', 'I-485', 'I-526',
+    'I-539', 'I-600', 'I-600A', 'I-601', 'I-601A', 'I-612', 'I-730', 'I-751', 'I-765', 'I-765V', 'I-800', 'I-800A',
+    'I-817', 'I-821', 'I-821D', 'I-824', 'I-829', 'I-914', 'I-918', 'I-924', 'I-929'
+]
+CENTER_NAMES = ['EAC', 'VSC', 'WAC', 'CSC', 'LIN', 'NSC', 'SRC', 'TSC', 'MSC', 'NBC', 'IOE', 'YSC']
+
+
+def get_case_id(center_name: str, two_digit_yr: int, day: int, code: int, case_serial_numbers: int) -> str:
+    return center_name + str(two_digit_yr) + str(day) + str(code) + (str(case_serial_numbers)).zfill(4)
+
+
+def get_form_type(text: str):
+    for form in FORM_TYPES:
+        if form in text:
+            return form
+    return 'unknown form type'
+
+
+def parse_response(response):
+    text = str(BeautifulSoup(response.text, 'html.parser').findAll('div', {'class': 'text-center'})[0])
+    status = re.search('(?<=<h1>).*(?=</h1>)', text)
+    if status is None or status[0] == '' or status.group(0) == '':
+        return None
+    else:
+        return status.group(0), get_form_type(text)
+
+
+def get_last_case_number(center_name: str, two_digit_yr: int, day: int, code: int):
+    if parse_response(requests.get(BASE_URL + get_case_id(center_name, two_digit_yr, day, code, 1))) is None:
+        return 0
+    low = 1
+    high = 9999
+    while low < high:
+        mid = int((low + high) / 2)
+        result = parse_response(requests.get(BASE_URL + get_case_id(center_name, two_digit_yr, day, code, mid)))
+        # find first empty response
+        if result is not None:
+            low = mid + 1
+        else:
+            high = mid
+    return low - 1
+
+
+def merge(counter: Counter):
+    with open('./data.json') as f:
+        current_counter = Counter(json.loads(f.read()))
+    with open('./data.json', 'w') as f:
+        for k in counter:
+            current_counter[k] += counter[k] - current_counter[k]
+        f.write(json.dumps(current_counter, sort_keys=True, indent=4))
+
+
+async def main(center_name: str, two_digit_yr: int, day: int, code: int, counter: Counter):
+    current_day_since_1970 = (date.today() - date(1970, 1, 1)).days
+    event_loop = asyncio.get_event_loop()
+
+    case_serial_numbers = range(1, get_last_case_number(center_name, two_digit_yr, day, code))
+    ids = [get_case_id(center_name, two_digit_yr, day, code, number) for number in case_serial_numbers]
+
+    futures = [event_loop.run_in_executor(None, requests.get, BASE_URL + case_id) for case_id in ids]
+    responses = [await f for f in futures]
+
+    for response in responses:
+        parsed = parse_response(response)
+        if parsed is not None:
+            status, form_type = parsed
+            counter[f'{center_name}|{two_digit_yr}|{day}|{code}|{form_type}|{status}|{current_day_since_1970}'] += 1
+    merge(counter)
+
+
+def run():
+    counter = Counter()
+    for day in range(168, 173):
+        for code in range(0, 10):
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main('WAC', 20, day, code, counter))
+
+
+run()
