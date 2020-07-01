bbc16d3aad23bf1648ad36f13ebea243857df0ac
Moyang He <iammoyang@gmail.com>
2020-07-01 01:46:12 -0700

    pie chart
    Former-commit-id: 470734040efcdd6c7cc60c2ded7600da590ee4c8

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import ColorHash from "color-hash";
 import Immutable from "immutable";
 import JSON5 from "json5";
 import nullthrows from "nullthrows";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 // @ts-ignore
 import { Comments, FacebookProvider } from "react-facebook";
 import {
   Bar,
   BarChart,
   CartesianGrid,
+  ContentRenderer,
   Line,
   LineChart,
+  Pie,
+  PieChart,
+  PieLabelRenderProps,
+  Sector,
   Tooltip,
   XAxis,
   YAxis,
+  Cell,
 } from "recharts";
 
 import FormControl from "@material-ui/core/FormControl";
@@ -23,10 +29,12 @@ import Grid from "@material-ui/core/Grid";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
 
 import WeChatDonation from "./donation_wechat.jpg";
-import WechatQR from "./wechat_qr.jpg";
 import WechatGroupQR from "./wechat_group_qr.jpg";
+import WechatQR from "./wechat_qr.jpg";
+import { TextField } from "@material-ui/core";
 
 const JSON5_URL =
   "https://raw.githubusercontent.com/vicdus/uscis-case-statistics/master/src/data.json5";
@@ -46,10 +54,16 @@ function App() {
     new URL(window.location.href).searchParams.get("form") ?? "I-129";
   const selectedCenter =
     new URL(window.location.href).searchParams.get("center") ?? "WAC";
+  const workday = Number.parseInt(
+    new URL(window.location.href).searchParams.get("workday") ?? "0"
+  );
+
   const [selectedUpdateDay, setSelectedUpdateDay] = useState<string | null>(
     null
   );
   const [caseData, setCaseData] = useState<Object>({});
+  const [activeStatus, setActiveStatus] = useState<number>(0);
+  const [caseID, setCaseID] = useState<string>("");
 
   const setSearchParam = (key: string, value: string) => {
     const url = new URL(window.location.href);
@@ -74,149 +88,342 @@ function App() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const entires = Immutable.List(
-    Object.entries(caseData).flatMap(([key, counts]) => {
-      const [center, year, day, code, form, status] = key.split("|");
-      return Object.entries(counts).map((count) => {
-        return {
-          center,
-          year,
-          day,
-          code,
-          form,
-          status,
-          updateDay: count[0] as string,
-          count: count[1] as number,
-        };
-      });
-    })
+  const entries = useMemo(
+    () =>
+      Immutable.List(
+        Object.entries(caseData).flatMap(([key, counts]) => {
+          const [center, year, day, code, form, status] = key.split("|");
+          return Object.entries(counts).map((count) => {
+            return {
+              center,
+              year,
+              day,
+              code,
+              form,
+              status,
+              updateDay: count[0] as string,
+              count: count[1] as number,
+            };
+          });
+        })
+      ),
+    [caseData]
   );
 
-  const selectedEntriesAllDate = entires.filter(
-    (e) => e.form === selectedForm && e.center === selectedCenter
-  );
-  const availableUpdateDays = selectedEntriesAllDate
-    .map((e) => Number.parseInt(e.updateDay))
-    .toSet()
-    .toList()
-    .sort();
-
-  const countValueForAllDays = selectedEntriesAllDate
-    .map((e) => e.count)
-    .toSet()
-    .toList()
-    .sort();
-
-  const latestUpdateDay = selectedEntriesAllDate
-    .map((e) => Number.parseInt(e.updateDay))
-    .max();
-
-  const selectedEntries = selectedEntriesAllDate.filter(
-    (e) => e.updateDay === (selectedUpdateDay ?? latestUpdateDay)?.toString()
+  const selectedEntriesAllDate = useMemo(
+    () =>
+      entries.filter(
+        (e) => e.form === selectedForm && e.center === selectedCenter
+      ),
+    [entries, selectedForm, selectedCenter]
   );
 
-  const formTypes = entires.map((e) => e.form).toSet();
-  const centerNames = entires.map((e) => e.center).toSet();
+  const availableUpdateDays = useMemo(
+    () =>
+      selectedEntriesAllDate
+        .map((e) => Number.parseInt(e.updateDay))
+        .toSet()
+        .toList()
+        .sort(),
+    [selectedEntriesAllDate]
+  );
 
-  const statusCount = selectedEntriesAllDate.countBy((x) => x.status);
-  const existStatus = selectedEntriesAllDate
-    .map((e) => e.status)
-    .toSet()
-    .toList()
-    .sortBy((s) => -(statusCount.get(s) ?? 0));
+  const countValueForAllDays = useMemo(
+    () =>
+      selectedEntriesAllDate
+        .map((e) => e.count)
+        .toSet()
+        .toList()
+        .sort(),
+    [selectedEntriesAllDate]
+  );
 
-  const exisitDays = selectedEntriesAllDate
-    .map((e) => Number.parseInt(e.day))
-    .toSet()
-    .toList()
-    .sort();
+  const latestUpdateDay = useMemo(
+    () => selectedEntriesAllDate.map((e) => Number.parseInt(e.updateDay)).max(),
+    [selectedEntriesAllDate]
+  );
 
-  const dataset = selectedEntries
-    .groupBy((e) => e.day)
-    .map((e, day) => {
-      return {
-        day,
-        ...e
-          .reduce(
-            (counter, v) => counter.set(v.status, v.count),
-            Immutable.Map<string, number>()
-          )
-          .toObject(),
-      };
-    })
-    .toList()
-    .sort((a, b) => Number.parseInt(a.day) - Number.parseInt(b.day))
-    .toArray();
+  const selectedEntries = useMemo(
+    () =>
+      selectedEntriesAllDate.filter(
+        (e) =>
+          e.updateDay === (selectedUpdateDay ?? latestUpdateDay)?.toString()
+      ),
+    [selectedEntriesAllDate, selectedUpdateDay, latestUpdateDay]
+  );
 
-  const datasetWithBackfill = exisitDays
-    .map((day) => dataset.find((v) => v.day === day.toString()) ?? { day })
-    .toArray();
+  const formTypes = useMemo(() => entries.map((e) => e.form).toSet(), [
+    entries,
+  ]);
+  const centerNames = useMemo(() => entries.map((e) => e.center).toSet(), [
+    entries,
+  ]);
 
-  const chart = (
-    <LineChart width={1440} height={810} data={datasetWithBackfill}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="day" />
-      <YAxis
-        type="number"
-        height={810}
-        domain={[0, countValueForAllDays.max() ?? 1]}
-      />
-      <Tooltip
-        offset={100}
-        itemSorter={(a) => -a.payload[nullthrows(a.dataKey?.toString())]}
-      />
-      {existStatus.toArray().map((s, ind) => (
-        <Line
-          key={ind}
-          type="linear"
-          isAnimationActive={false}
-          dataKey={s}
-          stroke={getColor(s)}
+  const statusCount = useMemo(
+    () => selectedEntriesAllDate.countBy((x) => x.status),
+    [selectedEntriesAllDate]
+  );
+  const existStatus = useMemo(
+    () =>
+      selectedEntriesAllDate
+        .map((e) => e.status)
+        .toSet()
+        .toList()
+        .sortBy((s) => -(statusCount.get(s) ?? 0)),
+    [selectedEntriesAllDate, statusCount]
+  );
+
+  const exisitDays = useMemo(
+    () =>
+      selectedEntriesAllDate
+        .map((e) => Number.parseInt(e.day))
+        .toSet()
+        .toList()
+        .sort(),
+    [selectedEntriesAllDate]
+  );
+
+  const dataset = useMemo(
+    () =>
+      selectedEntries
+        .groupBy((e) => e.day)
+        .map((e, day) => {
+          return {
+            day,
+            ...e
+              .reduce(
+                (counter, v) => counter.set(v.status, v.count),
+                Immutable.Map<string, number>()
+              )
+              .toObject(),
+          };
+        })
+        .toList()
+        .sort((a, b) => Number.parseInt(a.day) - Number.parseInt(b.day))
+        .toArray(),
+    [selectedEntries]
+  );
+
+  const datasetWithBackfill = useMemo(
+    () =>
+      exisitDays
+        .map(
+          (day) =>
+            dataset.find((v) => v.day === day.toString()) ?? {
+              day: day.toString(),
+            }
+        )
+        .toArray(),
+    [exisitDays, dataset]
+  );
+
+  const caseIDInput = (
+    <div>
+      <div style={{ width: "600px", display: "inline", float: "left" }}>
+        <TextField
+          id="standard-basic"
+          label="也可输入你的Case ID来查询你的号段下所有status的比例, 例如 WAC2017150172"
+          fullWidth={true}
+          onChange={(v) => setCaseID(v.target.value)}
         />
-      ))}
-    </LineChart>
+      </div>
+      <div style={{ width: "100px", display: "inline", float: "left" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          disabled={!Boolean(caseID.match(/^\w{3}\d{5}5\d{4}$/))}
+          onClick={() => setSearchParam("workday", caseID.substr(5, 3))}
+        >
+          {(() =>
+            caseID.match(/^\w{3}\d{5}5\d{4}$/)
+              ? "查询号段" + caseID.substr(5, 3)
+              : "Case ID invalid")()}
+        </Button>
+      </div>
+    </div>
   );
 
-  const barChart = (
-    <BarChart
-      height={1440}
-      width={810}
-      data={datasetWithBackfill}
-      layout="vertical"
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis
-        type="number"
-        dataKey="day"
-        domain={[0, countValueForAllDays.max() ?? 1]}
-      />
-      <YAxis
-        type="category"
-        dataKey="day"
-        domain={[(exisitDays.min() ?? 0) - 1, (exisitDays.max() ?? 1) + 1]}
-        tick={true}
-        interval={0}
-        allowDecimals={true}
-        ticks={exisitDays.toArray()}
-      />
-      <Tooltip
-        offset={100}
-        itemSorter={(a) =>
-          -existStatus.indexOf(nullthrows(a.dataKey) as string)
-        }
-      />
-      {existStatus.toArray().map((s, ind) => (
-        <Bar
-          key={ind}
-          isAnimationActive={false}
-          dataKey={s}
-          stackId="a"
-          fill={getColor(s)}
+  const renderActiveShape: ContentRenderer<PieLabelRenderProps> = (props) => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx,
+      cy,
+      name,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      payload,
+      percent,
+      value,
+    } = props;
+    const sin = Math.sin(-RADIAN * (midAngle as number));
+    const cos = Math.cos(-RADIAN * (midAngle as number));
+    const sx = (cx as number) + ((outerRadius as number) + 10) * cos;
+    const sy = (cy as number) + ((outerRadius as number) + 10) * sin;
+    const mx = (cx as number) + ((outerRadius as number) + 30) * cos;
+    const my = (cy as number) + ((outerRadius as number) + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? "start" : "end";
+
+    return (
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+          {payload.name}
+        </text>
+        <Sector
+          cx={cx as number}
+          cy={cy as number}
+          innerRadius={innerRadius as number}
+          outerRadius={outerRadius as number}
+          startAngle={startAngle as number}
+          endAngle={endAngle as number}
+          fill={fill}
         />
-      ))}
-    </BarChart>
-  );
+        <Sector
+          cx={cx as number}
+          cy={cy as number}
+          startAngle={startAngle as number}
+          endAngle={endAngle as number}
+          innerRadius={(outerRadius as number) + 6}
+          outerRadius={(outerRadius as number) + 10}
+          fill={fill}
+        />
+        <path
+          d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+          stroke={fill}
+          fill="none"
+        />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          textAnchor={textAnchor}
+          fill="#333"
+        >{`${name}`}</text>
+        <text
+          x={ex + (cos >= 0 ? 1 : -1) * 12}
+          y={ey}
+          dy={18}
+          textAnchor={textAnchor}
+          fill="#999"
+        >
+          {`${value} cases, ${((percent as number) * 100).toFixed(2)}%`}
+        </text>
+      </g>
+    );
+  };
 
+  const pieChart = useMemo(() => {
+    if (!workday) {
+      return null;
+    }
+    const pieData = Object.entries(
+      datasetWithBackfill.find((e) => e.day === workday.toString()) ?? {}
+    )
+      .map(([key, val]) => ({ name: key, value: Number.parseInt(val) }))
+      .filter((v) => v.name !== "day")
+      .sort((v) => v.value);
+
+    return (
+      <div style={{ display: "block" }}>
+        <PieChart width={800} height={800}>
+          <Pie
+            activeIndex={activeStatus}
+            activeShape={renderActiveShape}
+            dataKey="value"
+            isAnimationActive={false}
+            data={pieData}
+            cx={400}
+            cy={400}
+            outerRadius={200}
+            innerRadius={165}
+            onMouseEnter={(data, index) => setActiveStatus(index)}
+            fill="#8884d8"
+          >
+            {pieData.map((entry, index) => (
+              <Cell fill={getColor(entry.name)} />
+            ))}
+          </Pie>
+          <Tooltip offset={100} />
+        </PieChart>
+        <h2>以上饼图：号段{selectedCenter + "20" + workday + "5XXXX"}</h2>
+      </div>
+    );
+  }, [activeStatus, datasetWithBackfill, caseID, workday]);
+
+  const chart = useMemo(
+    () => (
+      <LineChart width={1440} height={810} data={datasetWithBackfill}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="day" />
+        <YAxis
+          type="number"
+          height={810}
+          domain={[0, countValueForAllDays.max() ?? 1]}
+        />
+        <Tooltip
+          offset={100}
+          itemSorter={(a) => -a.payload[nullthrows(a.dataKey?.toString())]}
+        />
+        {existStatus.toArray().map((s, ind) => (
+          <Line
+            key={ind}
+            type="linear"
+            isAnimationActive={false}
+            dataKey={s}
+            stroke={getColor(s)}
+          />
+        ))}
+      </LineChart>
+    ),
+    [datasetWithBackfill, existStatus, countValueForAllDays]
+  );
+  const barChart = useMemo(
+    () => (
+      <BarChart
+        height={1440}
+        width={810}
+        data={datasetWithBackfill}
+        layout="vertical"
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          type="number"
+          dataKey="day"
+          domain={[0, countValueForAllDays.max() ?? 1]}
+        />
+        <YAxis
+          type="category"
+          dataKey="day"
+          domain={[(exisitDays.min() ?? 0) - 1, (exisitDays.max() ?? 1) + 1]}
+          tick={true}
+          interval={0}
+          allowDecimals={true}
+          ticks={exisitDays.toArray()}
+        />
+        <Tooltip
+          offset={100}
+          itemSorter={(a) =>
+            -existStatus.indexOf(nullthrows(a.dataKey) as string)
+          }
+        />
+        {existStatus.toArray().map((s, ind) => (
+          <Bar
+            key={ind}
+            isAnimationActive={false}
+            dataKey={s}
+            stackId="a"
+            fill={getColor(s)}
+          />
+        ))}
+      </BarChart>
+    ),
+    [datasetWithBackfill, countValueForAllDays, exisitDays, existStatus]
+  );
   const introduction = (
     <div>
       <h1>USCIS case progress tracker</h1>
@@ -386,6 +593,8 @@ function App() {
       {introduction}
       {formTypeSelector}
       {centerSelector}
+      {caseIDInput}
+      {pieChart}
       {updateDayPicker}
       {barChart}
       {chart}
