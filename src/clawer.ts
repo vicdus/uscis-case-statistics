d44c0070ceeeaad797a944e4eda7bcc538c79def
Moyang He <iammoyang@gmail.com>
2020-08-25 01:49:31 -0700

    update
    Former-commit-id: 67fb5491ebec498aef44ea375cb98f6b54e8c71d

--- src/clawer.ts
+++ src/clawer.ts
@@ -157,7 +157,7 @@ const claw = async (
 };
 
 (async () => {
-  for (const d of lodash.range(145, 300)) {
+  for (const d of lodash.range(145, 350)) {
     await Promise.all(
       Constants.CENTER_NAMES.map((name) => claw(name, 20, d, 5))
     );
