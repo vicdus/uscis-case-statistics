9d097161a5dd13d9ff98f3ec60365df019bfffff
Moyang <iammoyang@gmail.com>
2021-01-02 18:41:11 -0800

    script

---
+++ src/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+scriptdir=`dirname "$BASH_SOURCE"`
+
+pushd $scriptdir
+
+git pull
+
+tsc clawer.ts
+
+node clawer.js
+
+git add -A
+
+git commit -m 'update'
+
+git push
+
+npm run deploy
