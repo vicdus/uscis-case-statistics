43f84fd76fa971619380b8c36579c309cd91dc93
Moyang <iammoyang@gmail.com>
2021-07-13 19:51:43 -0700

    go

--- src/scrapper/main.go
+++ src/scrapper/main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+var CENTER_NAMES = []string{
+	"WAC",
+	"EAC",
+	"VSC",
+	"CSC",
+	"LIN",
+	"NSC",
+	"SRC",
+	"TSC",
+	"MSC",
+	"NBC",
+	"IOE",
+	"YSC",
+}
 
 var FORM_TYPES = []string{
 	"I-90",
@@ -46,11 +65,16 @@ var FORM_TYPES = []string{
 }
 
 type Result struct {
-	Form   string
 	Status string
+	Form   string
 }
 
-func get(url string, c chan Result) {
+const (
+	center_year_day_code_serial = iota
+	center_year_code_day_serial
+)
+
+func get(url string) Result {
 	res, _ := http.Get(url)
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
@@ -61,20 +85,86 @@ func get(url string, c chan Result) {
 	status := body.Find("h1").Text()
 	for _, form := range FORM_TYPES {
 		if strings.Contains(bodyText, form) {
-			c <- Result{status, form}
+			return Result{status, form}
+		}
+	}
+	return Result{status, "unknown"}
+}
+
+func toURL(center string, two_digit_yr int, day int, code int, case_serial_numbers int) string {
+	return fmt.Sprintf("https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=%s%d%03d%d%04d", center, two_digit_yr, day, code, case_serial_numbers)
+}
+
+func clawAsync(center string, two_digit_yr int, day int, code int, case_serial_numbers int, format string, c chan Result) {
+	c <- claw(center, two_digit_yr, day, code, case_serial_numbers, format)
+}
+
+func claw(center string, two_digit_yr int, day int, code int, case_serial_numbers int, format string) Result {
+	return get(toURL(center, two_digit_yr, day, code, case_serial_numbers))
+}
+
+func getLastCaseNumber(center string, two_digit_yr int, day int, code int, format string) int {
+	low := 1
+	high := 1
+	for claw(center, two_digit_yr, day, code, high, format).Status != "" && high < 10000 {
+		high *= 2
+	}
+	for low < high {
+		mid := (low + high) / 2
+		if claw(center, two_digit_yr, day, code, mid, format).Status != "" {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return low
+}
+
+func all(center string, two_digit_yr int, day int, code int, format string) {
+	const url = "./data_go.json"
+	last := getLastCaseNumber(center, two_digit_yr, day, code, format)
+	fmt.Printf("loading %s total of %d at day %d\n", center, last, day)
+	c := make(chan Result)
+	epoch_day := time.Now().Unix() / 86400
+	for i := 1; i < last; i++ {
+		go clawAsync(center, two_digit_yr, day, code, i, "", c)
+	}
+	counter := make(map[string]map[int64]int)
+	for i := 1; i < last; i++ {
+		cur := <-c
+		key := fmt.Sprintf("%s|%d|%d|%d|%s|%s", center, two_digit_yr, day, code, cur.Form, cur.Status)
+
+		if counter[key] == nil {
+			counter[key] = make(map[int64]int)
+		}
+		counter[key][epoch_day] += 1
+	}
+	existingCounter := make(map[string]map[int64]int)
+	jsonFile, _ := os.ReadFile(url)
+	json.Unmarshal([]byte(jsonFile), &existingCounter)
+
+	getMerged(existingCounter, counter)
+	b, _ := json.MarshalIndent(existingCounter, "", "  ")
+	ioutil.WriteFile(url, b, 0666)
+	fmt.Printf("Done %s total of %d at day %d\n", center, last, day)
+}
+
+func getMerged(m1, m2 map[string]map[int64]int) {
+	for key, counter := range m2 {
+		if m1[key] == nil {
+			m1[key] = counter
+		} else {
+			for day, count := range counter {
+				m1[key][day] = count
+			}
 		}
 	}
 }
 
 func main() {
-	c := make(chan Result)
-	url := "https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=EAC2115950627"
-
-	for i := 0; i < 10; i++ {
-		go get(url, c)
-	}
-
-	for res := range c {
-		fmt.Println(res)
+	for day := 1; day < 356; day++ {
+		for _, name := range CENTER_NAMES {
+			all(name, 21, day, 5, "")
+		}
 	}
 }
