68c95e5e3868126406d81f1ffd78a02db9def3c2
Moyang <iammoyang@gmail.com>
2021-08-08 00:54:30 -0700

    ad
    Former-commit-id: d80475981ae29a07a5ccefb6925ce518256895a2

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Comments from "./Comments"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Comments />
+  </React.StrictMode>,
+  document.getElementById("comments")
 );
