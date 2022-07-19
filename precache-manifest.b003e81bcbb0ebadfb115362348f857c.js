87732abd1611659cfaf1d34b2ac9116e49abb91d
Moyang He <iammoyang@gmailcom>
2022-07-19 11:13:57 -0700

    Updates

---
+++ precache-manifest.b003e81bcbb0ebadfb115362348f857c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69709b10f3c2f3e6055f9e6c2a668f6c",
+    "url": "/uscis-case-statistics/index.html"
+  },
+  {
+    "revision": "f2e61e669f4324c76113",
+    "url": "/uscis-case-statistics/static/js/2.5e2dd237.chunk.js"
+  },
+  {
+    "revision": "00c5665da2ae4b537cd42f55c08c8b2e",
+    "url": "/uscis-case-statistics/static/js/2.5e2dd237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb540bba46d82ae830e2",
+    "url": "/uscis-case-statistics/static/js/3.5cb79601.chunk.js"
+  },
+  {
+    "revision": "b31c65e4943880189eb3",
+    "url": "/uscis-case-statistics/static/js/4.fc210a7b.chunk.js"
+  },
+  {
+    "revision": "dbb3d2362a7f5a246b3f",
+    "url": "/uscis-case-statistics/static/js/5.f5f9e5e9.chunk.js"
+  },
+  {
+    "revision": "ee2fd090e676b6f717f1",
+    "url": "/uscis-case-statistics/static/js/6.6a8f7f1b.chunk.js"
+  },
+  {
+    "revision": "4094fcf302e49ef3c157",
+    "url": "/uscis-case-statistics/static/js/7.4df610ef.chunk.js"
+  },
+  {
+    "revision": "5fc2bcc35308cc934d6e",
+    "url": "/uscis-case-statistics/static/js/8.7aec261c.chunk.js"
+  },
+  {
+    "revision": "0c70bd6ff1955ddc867e",
+    "url": "/uscis-case-statistics/static/js/main.9bc4863d.chunk.js"
+  },
+  {
+    "revision": "be318a98733c6e65e066",
+    "url": "/uscis-case-statistics/static/js/runtime-main.94ddfd62.js"
+  },
+  {
+    "revision": "e778d75a8e35422f3c7aaf943ada6e5c",
+    "url": "/uscis-case-statistics/static/media/case_number_format.e778d75a.png"
+  },
+  {
+    "revision": "84e04efe993c3656d06b9df04b20c2e2",
+    "url": "/uscis-case-statistics/static/media/donation_wechat.84e04efe.jpg"
+  },
+  {
+    "revision": "b7e75b32b069f3168a2adf534fb77758",
+    "url": "/uscis-case-statistics/static/media/monetization.b7e75b32.jpg"
+  },
+  {
+    "revision": "76bf9850aed2b35898dfd71034ae6960",
+    "url": "/uscis-case-statistics/static/media/venmo.76bf9850.png"
+  },
+  {
+    "revision": "b70b83ac95c8001b59cc7bc8ba1989f7",
+    "url": "/uscis-case-statistics/static/media/wechat_group_qr.b70b83ac.jpg"
+  },
+  {
+    "revision": "6a2dd925042336ac382873d0b0a12e7c",
+    "url": "/uscis-case-statistics/static/media/wechat_qr.6a2dd925.jpg"
+  }
+]);
