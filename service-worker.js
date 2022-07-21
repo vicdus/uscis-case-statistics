ad1c78edee7cbd37b9dc9daf38d13fe248dd87ee
Moyang He <iammoyang@gmailcom>
2022-07-21 11:12:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/uscis-case-statistics/precache-manifest.8404f4b4191767c68ff15f7d14d55fda.js"
+  "/uscis-case-statistics/precache-manifest.80493226d4ebe89c8c9edf7cc09437eb.js"
 );
 
 self.addEventListener('message', (event) => {
